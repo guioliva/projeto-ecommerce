2854a2b2f58d5ef59cbd0da7925fe84c62b3f096
[Moises Almeida] <[moisesssprg@gmail.com]>
2023-05-17 19:45:20 -0300

    pagina produto

--- pagina-produto/src/carousel.js
+++ pagina-produto/src/carousel.js
@@ -2,27 +2,30 @@ $(function () {
   $(".watch-slider").slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
     centerMode: true,
     prevArrow: $("#arrow-prev"),
     nextArrow: $("#arrow-next"),
     responsive: [
       {
-        breakpoint: 1268,
+        breakpoint: 1468,
         settings: {
           slidesToShow: 3,
         },
       },
       {
-        breakpoint: 820,
+        breakpoint: 850,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: false,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 550,
         settings: {
           slidesToShow: 1,
+          fade: true,
+          cssEase: "linear",
         },
       },
     ],
